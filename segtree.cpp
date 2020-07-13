19989e16a5e398d0c9a40517e6e89ec01d044c59
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2020-07-13 15:08:03 -0500

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,10 +1,10 @@
 #define MAXSEG 262144
 
-int N;
+int n;
 int st[MAXSEG];
 
 void update(int i, int x, int lo=0, int hi=-1, int node=0) {
-	if (hi == -1) hi = N - 1;
+	if (hi == -1) hi = n - 1;
 	if (hi < i || lo > i) return;
 	if (lo == hi) { st[node] += x; return; }
 	int mid = (lo + hi) / 2;
@@ -14,7 +14,7 @@ void update(int i, int x, int lo=0, int hi=-1, int node=0) {
 }
 
 int query(int s, int e, int lo=0, int hi=-1, int node=0) {
-	if (hi == -1) hi = N - 1;
+	if (hi == -1) hi = n - 1;
 	if (hi < s || lo > e) return 0;
 	if (lo >= s && hi <= e) return st[node];
 	int mid = (lo + hi) / 2;
@@ -24,9 +24,9 @@ int query(int s, int e, int lo=0, int hi=-1, int node=0) {
 
 
 int main() {
-	N = 100;
-	for (int i = 0; i < N; i++) {
+	n = 100;
+	for (int i = 0; i < n; i++) {
 		update(i, i + 1); // add i+1 to the i-th number (initially 0)
 	}
-	cout << query(0, N - 1) << endl;
+	cout << query(0, n - 1) << endl;
 }
