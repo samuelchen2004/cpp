13b39e9fcf26bd3c658999a7eb3b593f15d253de
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2020-09-16 14:22:58 -0500

    Update hopcroft_karp.cpp

--- hopcroft_karp.cpp
+++ hopcroft_karp.cpp
@@ -1,75 +1,51 @@
-void colorGraph(int x, bool c) {
-	if (!c) color[x] = 1;
-	else color[x] = 2;
-	vis[x] = 1;
-	trav(i,adj[x]) {
-		if (!vis[i]) {
-			colorGraph(i,!c);
+vector<int> adj[mxN];
+int match[mxN];
+int dist[mxN];
+
+// current node maximum at 1e5, change accordingly
+bool bfs() {
+	queue<int> q;
+	memset(dist, -1, sizeof dist);
+	for (int i = 0; i < 1e5; i++) {
+		if (match[i] == -1) {
+			q.push(i);
+			dist[i] = 0;
 		}
 	}
+	bool reached = false;
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		for (int v : adj[x]) {
+			if (match[v] == -1) reached = true;
+			else if (dist[match[v]] == -1) {
+				dist[match[v]] = dist[x] + 1;
+				q.push(match[v]);
+			}
+		}
+	}
+	return reached;
 }
-bool bfs(int m) {
-    queue<int> q;
-    forr(u,0,m+1) {
-        if (!pairU[u]) {
-            dist[u] = 0;
-            q.push(u);
-        }
-        else dist[u] = INT_MAX;
-    }
-    dist[0] = INT_MAX;
-    while (q.size()) {
-        int u = q.front();
-        q.pop();
-        if (dist[u] < dist[0]) {
-            trav(i,adj[u]) {
-                if (dist[pairV[i]] == INT_MAX) {
-                    dist[pairV[i]] = dist[u]+1;
-                    q.push(pairV[i]);
-                }
-            }
-        }
-    }
-    return dist[0] != INT_MAX;
+bool dfs(int x) {
+	if (x == -1) return true;
+	for (int v : adj[x]) {
+		if (match[v] == -1 || dist[match[v]] == dist[x] + 1) {
+			if (dfs(match[v])) {
+				match[v] = x, match[x] = v;
+				return true;
+			}
+		}
+	}
+	return false;
 }
-bool dfs(int u) {
-    if (u) {
-        trav(i,adj[u]) {
-            if (dist[pairV[i]] == dist[u]+1) {
-                if (dfs(pairV[i])) {
-                    pairV[i] = u;
-                    pairU[u] = i;
-                    return 1;
-                }
-            }
-        }
-        dist[u] = INT_MAX;
-        return 0;
-    }
-    return 1;
-}
-int hopcroftKarp(int n, int m) {
-    pairU = new int[m+1];
-    pairV = new int[n+1];
-    dist = new int[m+1];
-    forr(u,0,m+1) pairU[u] = 0;
-    forr(v,0,n+1) pairV[v] = 0;
-    int result = 0;
-    while (bfs(m)) {
-        forr(u,1,m+1) {
-            if (!pairU[u] && dfs(u)) result++;
-        }
-        return result;
-    }
-    return -1;
-}
-void solve() {
-    int nodes; // # nodes in graph
-    forr(i,0,nodes) if (!vis[i]) colorGraph(i,0);
-    int a = 0, b = 0;
-    trav(i,color) {
-        if (i == 1) a++;
-        if (i == 2) b++;
-    }
-    cout << "hopcroftKarp(max(a,b),min(a,b));
+int hopcroft_karp() {
+	memset(match, -1, sizeof match);
+
+	int matching = 0;
+	while (bfs()) {
+		for (int i = 0; i < 1e5; i++)
+			if (match[i] == -1 && dfs(i))
+				matching++;
+	}
+	return matching;
 }
