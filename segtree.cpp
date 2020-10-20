776444836c029b9560dd3300cb5fec592a35713e
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2020-10-20 15:19:32 -0500

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -16,6 +16,7 @@ void update(int i, int x, int lo=0, int hi=-1, int node=0) {
 	update(i, x, mid + 1, hi, 2 * node + 2);
 	st[node] = st[2 * node + 1] + st[2 * node + 2];
 }
+// for min segtree, remember to return INF instead of 0
 int query(int s, int e, int lo=0, int hi=-1, int node=0) {
 	if (hi == -1) hi = n - 1;
 	if (hi < s || lo > e) return 0;
@@ -24,6 +25,27 @@ int query(int s, int e, int lo=0, int hi=-1, int node=0) {
 	return query(s, e, lo, mid, 2 * node + 1) + 
 			query(s, e, mid + 1, hi, 2 * node + 2);
 }
+
+/* returns pair<value,index>
+can be used in min/max segtree */
+void update(int i, pi x, int lo=0, int hi=-1, int node=0) {
+	if (hi == -1) hi = n - 1;
+	if (hi < i || lo > i) return;
+	if (lo == hi) { st[node].f += x.f; st[node].s += x.s; return; }
+	int mid = (lo + hi) / 2;
+	update(i, x, lo, mid, 2 * node + 1);
+	update(i, x, mid + 1, hi, 2 * node + 2);
+	st[node] = max(st[2 * node + 1], st[2 * node + 2]);
+}
+pi query(int s, int e, int lo=0, int hi=-1, int node=0) {
+	if (hi == -1) hi = n - 1;
+	if (hi < s || lo > e) return {0,0};
+	if (lo >= s && hi <= e) return st[node];
+	int mid = (lo + hi) / 2;
+	return max(query(s, e, lo, mid, 2 * node + 1), 
+			query(s, e, mid + 1, hi, 2 * node + 2));
+}
+
 /* smallest index in the range [l,r] such that arr[idx] > x
 to change to next_smaller
 - (st[node] <= x) -> (st[node] >= x)
