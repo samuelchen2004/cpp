1267dbcb33c40438601b1ed1499296d873f52674
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2020-11-08 17:10:57 -0600

    Update segtree.cpp

--- segtree.cpp
+++ segtree.cpp
@@ -1,11 +1,9 @@
-#define MAXSEG 262144
-
 /* if n is not the max index of the segtree (specifically, if n is too small)
 initialize N and make segtree dependent on N
 currently, segtree is dependent on n */
 
 int n;
-int st[MAXSEG];
+int st[4*mxN];
 
 void update(int i, int x, int lo=0, int hi=-1, int node=0) {
 	if (hi == -1) hi = n - 1;
@@ -19,6 +17,42 @@ void update(int i, int x, int lo=0, int hi=-1, int node=0) {
 // for min segtree, remember to return INF instead of 0
 int query(int s, int e, int lo=0, int hi=-1, int node=0) {
 	if (hi == -1) hi = n - 1;
+	if (hi < s || lo > e) return 0;
+	if (lo >= s && hi <= e) return st[node];
+	int mid = (lo + hi) / 2;
+	return query(s, e, lo, mid, 2 * node + 1) + 
+			query(s, e, mid + 1, hi, 2 * node + 2);
+}
+
+// lazy propagation
+int lazy[4*mxN];
+
+void push(int lo, int hi, int node) {
+	if (lazy[node] == 0) return;
+	st[node] += (hi - lo + 1) * lazy[node];
+	if (lo != hi) {
+		lazy[2 * node + 1] += lazy[node];
+		lazy[2 * node + 2] += lazy[node];
+	}
+	lazy[node] = 0;
+}
+void update_range(int s, int e, int x, int lo=0, int hi=-1, int node=0) {
+	if (hi == -1) hi = n - 1;
+	push(lo, hi, node);
+	if (hi < s || lo > e) return;
+	if (lo >= s && hi <= e) {
+		lazy[node] = x;
+		push(lo, hi, node);
+		return;
+	}
+	int mid = (lo + hi) / 2;
+	update_range(s, e, x, lo, mid, 2 * node + 1);
+	update_range(s, e, x, mid + 1, hi, 2 * node + 2);
+	st[node] = st[2 * node + 1] + st[2 * node + 2];
+}
+int query(int s, int e, int lo=0, int hi=-1, int node=0) {
+	if (hi == -1) hi = n - 1;
+	push(lo, hi, node);
 	if (hi < s || lo > e) return 0;
 	if (lo >= s && hi <= e) return st[node];
 	int mid = (lo + hi) / 2;
