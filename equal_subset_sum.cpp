abd6c03f73e213871b811303e7da5d7dea4dd98d
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2020-12-24 22:11:39 -0600

    Update equal_subset_sum.cpp

--- equal_subset_sum.cpp
+++ equal_subset_sum.cpp
@@ -2,7 +2,7 @@ pair<vi,vi> partition(vi &v) {
 	int sum = accumulate(all(v),0);
 	if (sum % 2) return {vi(),vi()};
 	sum /= 2;
-	bitset<x> pos[1005];
+	bitset<x> pos[v.size()+5];
 	pos[0][0] = 1;
 	forr(i,0,v.size()) pos[i+1] = pos[i] | pos[i] << v[i];
 	if (!pos[v.size()][sum]) return {vi(),vi()};
