f1bcd3e380996e2f971ce2abc7e038d1978f6ea0
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2020-12-25 20:00:47 -0600

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -1,11 +1,10 @@
-ll fac[mxN];
-ll invFac[mxN];
+ll fac[mxN], invFac[mxN];
 
 void initFac() {
 	fac[0] = 1, invFac[0] = 1;
-    for (int i = 1; i < mxN; i++) {
-        fac[i] = (fac[i-1]*i)%MOD;
-        invFac[i] = modInv(fac[i]);
+	forr(i,1,mxN) {
+		fac[i] = (i*fac[i-1]) % MOD;
+		invFac[i] = modInv(fac[i]);
     }
 }
 ll C(ll n,ll r) {ll res = (((fac[n]*invFac[n-r])%MOD)*invFac[r])%MOD; return res;}
