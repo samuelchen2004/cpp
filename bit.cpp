f580db4b1a761712c526253407cd051f185b0afa
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2021-01-06 10:47:39 -0600

    Create bit.cpp

---
+++ bit.cpp
@@ -0,0 +1,26 @@
+int n, arr[mxN], bit[mxN];
+
+void update(int i, int x) {
+	int j = i+1;
+	for (; j < n+1; j += j&-j) {
+		bit[j-1] += x;
+	}
+}
+int get(int i) {
+	int ret = 0, j = i+1;
+	for (; j > 0; j -= j&-j) {
+		ret += bit[j-1];
+	}
+	return ret;
+}
+int query(int i, int j) {
+	return get(j) - (i ? get(i-1) : 0);
+}
+int main() {
+	cin >> n;
+	forr(i,0,n) {
+		cin >> arr[i];
+		update(i,arr[i]);
+	}
+	cout << query(0,n-1) << "\n";
+}
