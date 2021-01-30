1627643518ba513c11e4a94f2e065a63377e0b92
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2021-01-30 00:49:03 -0600

    Update toposort.cpp

--- toposort.cpp
+++ toposort.cpp
@@ -1,18 +1,18 @@
-vi adj[mxN];
-bool vis[mxN];
+int n, vis[mxN]; // vis[] is int to store {0,1,2}
+vi adj[mxN], topo;
+bool cycle;
 
 void dfs(int x) {
-	trav(i,adj[x]) if (!vis[i]) {
-		vis[i] = 1;
-		dfs(i);
+	vis[x] = 1;
+	trav(i,adj[x]) {
+		if (vis[i] == 1) {cycle = 1; return;}
+		if (!vis[i]) dfs(i);
 	}
+	vis[x] = 2;
 	topo.pb(x);
 }
 void solve() {
-	forr(i,0,n) if (!vis[i]) {
-		vis[i] = 1;
-		dfs(i);
-	}
+	forr(i,0,n) if (!vis[i]) dfs(i);
 	reverse(all(topo));
 	trav(i,topo) cout << i << " ";
 }
