d36c9499167402de1949048e24140f84c882173a
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2021-02-14 13:33:22 -0600

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,8 +1,8 @@
-vi sieve(int n) {
-	bool prime[n+1]; fill(prime,prime+n+1,1);
-	for (int p = 2; p*p <= n; p++) if (prime[p]) {
-		for (int i = p*p; i <= n; i += p) prime[i] = 0;
+vi sieve(int N) {
+	bool prime[N+1]; fill(prime,prime+N+1,1);
+	for (int p = 2; p*p <= N; p++) if (prime[p]) {
+		for (int i = p*p; i <= N; i += p) prime[i] = 0;
 	}
-	vi v; forr(p,2,n+1) if (prime[p]) v.pb(p);
+	vi v; forr(p,2,N+1) if (prime[p]) v.pb(p);
 	return v;
 }
