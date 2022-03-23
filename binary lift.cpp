6b920c49d69f9ce1486e9fa76495c7b51f50440d
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2022-03-22 23:18:20 -0500

    Create binary lift.cpp

---
+++ binary lift.cpp
@@ -0,0 +1,10 @@
+int up[mxN][25]; // tree is rooted at 1, not 0
+
+void init_lift() {
+	forr(i,1,25) forr(j,1,n+1) up[j][i] = up[up[j][i-1]][i-1];
+}
+int lift(int x, int k) {
+	forr(i,0,25) if ((k >> i) & 1) x = up[x][i];
+	dbg(x);
+	return x ?: -1;
+}
