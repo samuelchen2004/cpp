be1e7e24ee4f6c05506622c4735a0dcf514d3c54
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2022-03-23 18:28:04 -0500

    Create lca.cpp

---
+++ lca.cpp
@@ -0,0 +1,28 @@
+int depth[mxN], up[mxN][25];
+vi adj[mxN];
+
+void init_lift(int x = 0, int p = -1) {
+	forr(i,0,25) {
+		if (!i) up[x][i] = p;
+		else up[x][i] = up[up[x][i-1]][i-1];
+	}
+	if (p != -1) depth[x] = depth[p]+1;
+	trav(u,adj[x]) if (u != p) init_lift(u,x);
+}
+int lift(int x, int k) {
+	forr(i,0,25) if ((k >> i) & 1) x = up[x][i];
+	return x;
+}
+int lca(int a, int b) {
+	if (depth[a] < depth[b]) swap(a,b);
+	a = lift(a, depth[a]-depth[b]);
+	if (a == b) return a;
+	ford(i,25,0) {
+		int nxa = up[a][i], nxb = up[b][i];
+		if (nxa != nxb) a = nxa, b = nxb;
+	}
+	return up[a][0];
+}
+int dist(int a, int b) {
+	return depth[a] + depth[b] - 2*depth[lca(a,b)];
+}
