47541ea87e44b1628ca6c5bbf52d8dd3cee1b3f5
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2022-03-23 19:55:50 -0500

    Update binary lift.cpp

--- binary lift.cpp
+++ binary lift.cpp
@@ -1,9 +1,17 @@
-int up[mxN][25]; // tree is rooted at 1, not 0
+// mainly for forests, lca template already has binary lift
+
+int up[mxN][25]; // reminder: set up[0][0] = -1 if needed
 
 void init_lift() {
-	forr(i,1,25) forr(j,1,n+1) up[j][i] = up[up[j][i-1]][i-1];
+	forr(i,1,25) forr(j,0,n) {
+		if (up[j][i-1] >= 0) up[j][i] = up[up[j][i-1]][i-1];
+		else up[j][i] = -1;
+	}
 }
 int lift(int x, int k) {
-	forr(i,0,25) if ((k >> i) & 1) x = up[x][i];
-	return x ?: -1;
+	forr(i,0,25) if ((k >> i) & 1) {
+		x = up[x][i];
+		if (x == -1) return x;
+	}
+	return x;
 }
