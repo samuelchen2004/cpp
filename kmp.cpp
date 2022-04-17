4b831d0ddadfc203f30b705cf298dd5e1df5041e
samuelchen2004 <51966112+samuelchen2004@users.noreply.github.com>
2022-04-17 18:34:06 -0500

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -27,6 +27,27 @@ int kmp(str& s, str& w) {
 }
 void solve() {
 	cin >> s >> w;
-	cout << kmp(s,w) << "\n";
+	cout << kmp(s,w) << "\n"; // first occurrence of w in s
 	cout << s.substr(kmp(s,w),w.length()) << "\n";
+}
+
+
+
+
+
+// benq's kmp
+
+vi init_kmp(str s) {
+	int N = sz(s); vi f(N+1); f[0] = -1;
+	forr(i,1,N+1) {
+		for (f[i] = f[i-1]; f[i] != -1 && s[f[i]] != s[i-1];) f[i] = f[f[i]];
+		f[i]++;
+	}
+	return f;
+}
+// returns all occurrences of a in b
+vi kmp(str a, str b) {
+	vi f = init_kmp(a+"@"+b), ret;
+	forr(i,sz(a),sz(b)+1) if (f[i+sz(a)+1] == sz(a)) ret.pb(i-sz(a));
+	return ret;
 }
